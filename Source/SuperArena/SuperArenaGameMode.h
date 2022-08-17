539e1fb9ec97530f50ea45f5845c1a01a4aea553
Yorda <921864047@qq.com>
2022-08-17 16:48:18 +0900

    project file first uploaded

---
+++ Source/SuperArena/SuperArenaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperArenaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASuperArenaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASuperArenaGameMode();
+};
+
+
+
